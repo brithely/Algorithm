e26392bfd604b4d3a192c9274c60cfa0c1dd3fec
Jung Seung Ryoung <bing9013@naver.com>
2023-02-13 01:18:04 +0900

    solved leetcode no.14

---
+++ Algorithm/14. Longest Common Prefix.py
@@ -0,0 +1,26 @@
+"""
+2023.02.13
+https://leetcode.com/problems/longest-common-prefix/
+Easy
+"""
+
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        max_length = max(map(len, strs))
+        result = ""
+        for i in range(max_length):
+            for j, s in enumerate(strs):
+                try:
+                    if j == 0:
+                        temp = s[i]
+                    if temp != s[i]:
+                        temp = ""
+                        break
+                except IndexError:
+                    temp = ""
+                    break
+            if temp == "":
+                break
+            else:
+                result += temp
+        return result
