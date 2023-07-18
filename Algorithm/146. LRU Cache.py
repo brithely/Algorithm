7cae005d36bec0cd9312f5a8c87ee3311d6a7ebc
Jung Seung Ryoung <bing9013@naver.com>
2023-07-18 21:12:18 +0900

    solved leetcode no.146 twice

--- Algorithm/146. LRU Cache.py
+++ Algorithm/146. LRU Cache.py
@@ -26,3 +26,31 @@ class LRUCache:
         elif len(self.cache_dict) >= self.capacity:
             self.cache_dict.popitem(last=False)
         self.cache_dict[key] = value
+
+
+"""
+2023.07.18
+https://leetcode.com/problems/lru-cache/
+Medium
+"""
+
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.order_dict = OrderedDict()
+        self.capacity = capacity
+        
+
+    def get(self, key: int) -> int:
+        if self.order_dict.get(key) is not None:
+            self.order_dict.move_to_end(key)
+        return self.order_dict.get(key, -1)
+        
+
+    def put(self, key: int, value: int) -> None:
+        if self.order_dict.get(key) is not None:
+            self.order_dict.move_to_end(key)
+        elif len(self.order_dict) >= self.capacity:
+            self.order_dict.popitem(last=False)
+        self.order_dict[key] = value
+        
